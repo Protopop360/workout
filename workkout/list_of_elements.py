b03d6e730f26bcef23064912ae025bcc62134d16
Protopop360 <32513047+Protopop360@users.noreply.github.com>
2017-12-18 17:21:50 +0300

    Add files via upload

---
+++ workkout/list_of_elements.py
@@ -0,0 +1,45 @@
+dynamics = 'Ниже приведен список элементов динамики по возрастанию уровня сложности: \n 1. Базовые \n2. Средней сложности \n3.Высший пилотаж'
+level_1 = 'Первый уровень включает в себя выполнение таких элементов как:\n  1. Склепка\n  2.Стульчик\n  3.Выход из под турника\n 4.   '
+level_2 =  'Элементы среднего уровня:\n 1.Солнышко\n 2.Луна\n 3.360\n4.540 \n6. х-вылет\n' 
+level_3 = 'Да ты опасный парень!)\n Итак, элементами топового уровня являются:\n 1. 540 над туркником \n2. Петля Корбут \n3. Переднее сальто в зацеп\n'
+freerun = 'Разного рода соскоки с  турника:  1. Лач-гейнер 2. Переднее сальто 3. Лач гейнер 360 4. '
+
+
+dynamics_tutorials1 = {
+	"Склепка":'https://www.youtube.com/watch?v=RAKfotJ9nDM',
+	"Стульчик":'https://www.youtube.com/watch?v=7bhaeLYntbk&list=PL4021A48D28F22E5A&index=58',
+	"Выход из под турника":'https://www.youtube.com/watch?v=iuEPScYq_7E&list=PL4021A48D28F22E5A&index=70',
+	"Смертник":'https://www.youtube.com/watch?v=Q5eM9iW6_Ow',
+	"Топорик":'https://www.youtube.com/watch?v=uRbdN8xr6fo'
+
+	
+
+}
+dynamics_tutorials2  = {
+	"Солнышко":'https://www.youtube.com/watch?v=kQ9AYmg36QY',
+	"Луна":'https://www.youtube.com/watch?v=sQ1dvWRS_ZI',
+	"360":'https://www.youtube.com/watch?v=_C_pvL9hGto',
+	"540":'https://www.youtube.com/watch?v=yCl62NckN0Q',
+	"х-вылет":'https://www.youtube.com/watch?v=RgurfNaBzOk&list=PL4021A48D28F22E5A&index=10'
+
+
+
+}
+dynamics_tutorials3 = {
+	"Петля Корбут":'https://www.youtube.com/watch?v=LZcqBm4wJY0&list=PL4021A48D28F22E5A&index=6',
+	"540 над турником":'',
+	"Переднее сальто в зацеп":''
+
+}
+static_tutorials = {
+	"Горизонт":'https://www.youtube.com/watch?v=CVAXtIh4RlA',
+	"Подтягивания на одной":'https://www.youtube.com/watch?v=QG-lRNOjAk0',
+	"Передний вис":'https://www.youtube.com/watch?v=Gn2-6B028dc',
+	"Самолет":'https://www.youtube.com/watch?v=7dmhT8CvWjY',
+	"Ласточка":'https://www.youtube.com/watch?v=Q_kO7eq-XxU',
+	"Стойка на руках":'https://www.youtube.com/watch?v=MZK7yTa7QME',
+	"Спичаг":'https://www.youtube.com/watch?v=Ky1Cvqmxz-s',
+	"Высокий угол":'https://www.youtube.com/watch?v=_BU6p_w0TQE'
+
+}
+
